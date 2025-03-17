798a5d578cbe72909e72fcd034071a4b0dc053d0
puzzlelvr12 <marie.0526h@gmail.com>
2025-03-17 13:07:18 -0500

    feat: added resources page for specific issues

---
+++ app/pages/resourcesPage/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function ResourcesPage({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="flex min-h-screen flex-col">
+            <main className="flex flex-1 flex-col">
+                {children}
+            </main>
+        </div>
+    );
+}

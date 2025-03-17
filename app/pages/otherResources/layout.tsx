0bf53efd1709953b02485b6187226104e2a1b2ec
puzzlelvr12 <marie.0526h@gmail.com>
2025-03-17 13:19:26 -0500

    feat: created other resources page

--- app/pages/otherResources/layout.tsx
+++ app/pages/otherResources/layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from '../../../Components/Navbar';
 
 
 export default function RootLayout({
@@ -10,7 +9,6 @@ export default function RootLayout({
     return (
         <div className="min-h-screen">
             <main className="container mx-auto px-4">
-                <Navbar/>
                 {children}
             </main>
         </div>
